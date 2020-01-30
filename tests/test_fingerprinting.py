807ae556ed5d28585b9f4ff589c1d0953ddbfdde
dave36 <davlorenzo@hotmail.com>
2020-01-30 11:14:22 +0100

    Travis.yml updated to fix build failing

--- tests/test_fingerprinting.py
+++ tests/test_fingerprinting.py
@@ -16,24 +16,24 @@ class FingerprintingTestSuite(unittest.TestCase):
     """Footprinting test cases."""
 
     """Test to check the nmap scan to obtain the open ports"""
-    #def test_nmap_scan(self):
-    #    target = "127.0.0.1"
-    #    result = scan_open_ports(target)
-    #    self.assertIsNotNone(result)
+    def test_nmap_scan(self):
+        target = "127.0.0.1"
+        result = scan_open_ports(target)
+        self.assertIsNotNone(result)
 
     """Test to check the nmap scan to obtain the open ports with wrong string input"""
-    #def test_nmap_scan_wrong_input(self):
-    #    target = "a"
-    #    result = scan_open_ports(target)
-    #    self.assertIsNotNone(result)
-    #    self.assertFalse("ports" in result["scan"])
+    def test_nmap_scan_wrong_input(self):
+        target = "a"
+        result = scan_open_ports(target)
+        self.assertIsNotNone(result)
+        self.assertFalse("ports" in result["scan"])
     
     """Test to check the nmap scan to obtain the open ports with wrong integer input"""
-    #def test_nmap_scan_wrong_input_2(self):
-    #    target = "1231231"
-    #    result = scan_open_ports(target)
-    #    self.assertIsNotNone(result)
-    #    self.assertFalse("ports" in result["scan"])
+    def test_nmap_scan_wrong_input_2(self):
+        target = "1231231"
+        result = scan_open_ports(target)
+        self.assertIsNotNone(result)
+        self.assertFalse("ports" in result["scan"])
     
     """Test to check the plc scan to obtain the information about PLCs
         On this IP it wont't be running any PLC"""
